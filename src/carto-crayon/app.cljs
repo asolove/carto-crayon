dd07ff88fbb5a0f355108a0c030feb67a0772e93
Adam Solove <asolove@gmail.com>
2013-12-22 14:37:43 -0500

    Clean up component dependencies, working with path more correctly.

--- src/carto-crayon/app.cljs
+++ src/carto-crayon/app.cljs
@@ -1,16 +1,9 @@
 (ns carto-crayon.app
-  (:require-macros [cljs.core.async.macros :refer [go alt!]]
-                   [secretary.macros :refer [defroute]])
-  (:require [goog.events :as events]
-            [cljs.core.async :refer [put! <! >! chan timeout]]
-            [markdown.core :as md]
+  (:require-macros [cljs.core.async.macros :refer [go alt!]])
+  (:require [cljs.core.async :refer [put! <! >! chan timeout]]
             [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [secretary.core :as secretary]
-            [cljs-http.client :as http]
-            [carto-crayon.utils :refer [guid]])
-  (:import [goog History]
-           [goog.history EventType]))
+            [cljs-http.client :as http]))
 
 (enable-console-print!)
 
@@ -18,77 +11,83 @@
 ;; Data
 
 (def app-state
-  (atom {:selection {:layer "States"}
+  (atom {:selected false
          :layers []}))
-
-(defn get-layer [app name]
-  (first (filter #(= name (:name %)) (:layers app))))
-
-(defn get-feature [layer id]
-  (first (filter #(= id (:id %)) (:features layer))))
-
-(defn selected-layer [app]
-  (get-layer app (get-in app [:selection :layer])))
-
-(defn selected-feature [app]
-  (let [layer (selected-layer app)]
-    (when layer
-      (get-feature layer (get-in app [:selection :feature])))))
 
 (defn columns [features]
   (disj (into #{} (apply concat (map keys features))) :geometry))
 
 (defn select-layer [app layer]
-  (om/update! app [:selection]
-              #(merge % {:layer layer :feature nil})))
+  (om/update! app [:selected] (constantly layer)))
 
-(defn select-feature [app feature]
-  (print "selecting feature!" feature)
-  (om/update! app [:selection :feature]
+(defn select-feature [layer feature]
+  (om/update! layer [:selection]
               (constantly feature)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; UI helpers
 
-(defn feature-row [feature cols app]
-  (dom/tr #js {:onClick #(select-feature app (:id feature))
-               :className (when (= (:id feature) (get-in app [:selection :feature])) "selected")}
-          (into-array (map #(dom/td nil (feature %)) cols))))
-
-(defn features-table [app options]
-  (let [layer (selected-layer app)
-        features (:features layer)
-        cols (columns features)]
+(defn feature-row [layer {:keys [cols n]}]
+  (let [feature (get-in layer [:features n])
+        selected (= (:selected layer) n)]
     (om/component
-      (dom/div #js {:id "features"}
-               (dom/h2 nil "Features")
-               (dom/table nil
-                          (dom/thead nil (dom/tr nil
-                                                 (into-array
-                                                   (map #(dom/th nil (name %)) cols))))
-                          (dom/tbody nil (into-array
-                                           (map #(feature-row % cols app) features))))))))
+     (dom/tr #js {:onClick #(select-feature layer n)
+                  :className (when selected "selected")}
+             (into-array (map #(dom/td nil (feature %)) cols))))))
 
-(defn layer
-  [app {{:keys [name]} :layer}]
-  (let [selected (= name (:layer (:selection app)))]
-    (om/component
-      (dom/li #js {:className (when selected "selected") :onClick #(select-layer app name)} name))))
+(defn feature-style [feature])
 
-(defn layer-list [app]
-  (om/component
-    (dom/div #js {:id "layers"}
-             (dom/h2 nil "Layers")
-             (dom/ol nil (into-array
-                           (map #(om/build layer app {:opts {:layer %}})
-                                (:layers app)))))))
-
-(defn feature-style [feature]
+(defn feature-styles [feature]
   (om/component
     (dom/div #js {:id "styles"}
              (dom/h2 nil "Styles")
-             (dom/h1 nil (str (:id feature))))))
+             (dom/div nil
+                      (into-array
+                        (map #(om/build feature-style {:opts {:feature feature :style (key %)}})
+                             (:styles feature)))))))
 
+(defn layer-features [layer opts]
+  (om/component
+    (dom/div nil
+             (dom/div #js {:id "features"}
+                      (dom/h2 nil "Features")
+
+                      (if layer
+                        (let [features (:features layer)
+                              cols (columns features)]
+                          (dom/table nil
+                                     (dom/thead nil (dom/tr nil
+                                                            (into-array
+                                                             (map #(dom/th nil (name %)) cols))))
+                                     (dom/tbody nil (into-array
+                                                     (map #(om/build feature-row
+                                                                     layer
+                                                                     {:opts {:cols cols :n %}})
+                                                          (range (count features)))))))
+                        (dom/h2 nil "No layer")))
+             (om/build feature-styles layer {:path [:features (:selection layer)]}))))
+
+(defn layer-list-item
+  [app {:keys [n]}]
+  (om/component
+    (let [layer (get-in app [:layers n])
+          selected (= n (:selected app))]
+      (dom/li #js {:className (if selected "selected" "not-selected") :onClick #(select-layer app n)}
+              (:name layer)))))
+
+(defn layer-list [app]
+  (om/component
+    (let [selected (:selected app)]
+      (dom/div #js {:id "layers"}
+               (dom/h2 nil "Layers")
+               (dom/ol nil (into-array
+                             (map #(om/build layer-list-item
+                                             app
+                                             {:opts {:n %}})
+                                  (range (count (:layers app))) )))))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Map UI
 
 (def leaflet-map (atom false))
 
@@ -124,6 +123,9 @@
                (into-array (map #(om/build map-layer %) (:layers app)))))))
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; App bootstrap
+
 (defn carto-crayon-ui [app]
   (reify
     om/IRender
@@ -131,23 +133,19 @@
       (dom/div nil nil
                (dom/header #js {:className "auth"})
 
-               (let [feature (selected-feature app)]
-                 (dom/div #js {:id "data"}
-                          (om/build layer-list app)
-                          (om/build features-table app)
-                          (om/build feature-style feature)))
+               (dom/div #js {:id "data"}
+                        (om/build layer-list app)
+                        (om/build layer-features app {:path [:layers (:selected app)]}))
 
                (om/build map-view app)))))
 
-(def topology (atom 1))
-
+; Load initial data
 (go (let [districtTopology (js/JSON.parse (:body (<! (http/get "data/cd113.topojson"))))
           districtGeoJSON (js/topojson.feature districtTopology (.-cd113 (.-objects districtTopology)))
           ; convert everything but the geometry to edn
           districtLayer { :name "Districts"
                           :features (vec (.map (.-features districtGeoJSON)
                                                (fn [f] {:id (.-id f) :geometry (.-geometry f)})))}]
-      (swap! topology (constantly districtTopology))
       (swap! app-state (fn [state]
                          (update-in state [:layers]
                                     #(conj % districtLayer))))
